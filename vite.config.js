afd653a40e8c4fd71b993c174abd4c5d810a9772
Mengyun Li <lmy-lee1@outlook.com>
2025-10-26 14:49:11 +0800

    debug init test pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tis-nj.github.io/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
