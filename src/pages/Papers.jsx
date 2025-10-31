7cfffe1220a137d80eaf44bea3c7691d039793d1
Mengyun Li <lmy-lee1@outlook.com>
2025-10-31 11:28:37 +0800

    研究成果页面修改

--- src/pages/Papers.jsx
+++ src/pages/Papers.jsx
@@ -2,15 +2,15 @@ export default function Papers() {
   return (
     <div className="container">
       <h1>论文</h1>
-      <p>我们团队发表的重要论文：</p>
+      <p>团队成立后成员发表的重要论文（团队成员的其他论文可以从<a href="/members">团队成员</a>的个人主页查看）：</p>
       <ul>
         <li>《增量SAT/SMT问题的求解与应用综述》- 软件学报(CCFA) </li>
         <li>Revisiting Transferable Adversarial Images: Systemization, Evaluation, and New Insights - TPAMI(CCFA)</li>
         <li>Rust-doctor: Enhanced Feature for Rust Ownership and Lifetime Repair with Balanced Training Data Generation - ACL-EMNLP(CCFB)</li>
         <li>Function Clustering-Based Fuzzing Termination: Toward Smarter Early Stopping - IEEE-ASE(CCFA)</li>
-        {/* <li>Provable Watermarking for Data Poisoning Attacks - NIPS2025(CCFA)</li>
+        <li>Provable Watermarking for Data Poisoning Attacks - NIPS2025(CCFA)</li>
         <li>SeCon-RAG: A Two-Stage Semantic Filtering and Conflict-Free Framework for Trustworthy RAG - NIPS2025(CCFA)</li>
-        <li>Analyzing the Power of Chain of Thought through Memorization Capabilities - NIPS2025(CCFA)</li> */}
+        <li>Analyzing the Power of Chain of Thought through Memorization Capabilities - NIPS2025(CCFA)</li>
       </ul>
     </div>
   )
